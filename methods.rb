c4d865b0f27169cd831ccc9394810e02e62c4351
Diana C Peterson <dpeter10@gmail.com>
2023-01-09 18:40:10 -0500

    commit tree 6493be65321eadd6c8872393adebd8693456cd97

--- methods.rb
+++ methods.rb
@@ -1,1 +1,17 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+puts "Hello, #{name}!"
+end
+greet("Naureen!")
+
+def greet_with_default(name)
+puts "Hello, #{name}!"
+end
+greet("Naureen!")
+
+def greet_with_default
+    puts "Hello, programmer!"
+end
